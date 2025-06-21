6b27e425004e6f45f98a91f423013db452defa84
Koji Wong <kojirwong@gmail.com>
2025-06-21 02:01:30 -0700

    feat: thumbnail, better sending optics

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ import About from "@/components/sections/About";
 import Experience from "@/components/sections/Experience";
 import Projects from "@/components/sections/Projects";
 import Skills from "@/components/sections/Skills";
-
 const Home = () => {
   return (
     <div className="min-h-screen bg-luxurious-black text-white">
